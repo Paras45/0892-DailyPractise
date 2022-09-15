7230ffbf2fc64ff9505b79742b4c69d039d9b40c
Paras Ramola <paras0892.be20@chitkara.edu.in>
2022-09-15 11:15:49 +0530

    15 sept

--- routing_react/routing/src/App.js
+++ routing_react/routing/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Form from './Form';
 import Test from './Test'; 
+import Home from './Home2';
+import './App.css';
 const App=()=> {
   return (
-    <>
-    
-    <Test/>
-    </>
+    <div className="App">
+      <Home></Home>
+      <h1>Hello World</h1>
+      <h2>H2 heading</h2>
+      
+      
+    </div>
   )
 }
 
